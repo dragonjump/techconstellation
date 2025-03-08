97adc004a799e14552e7c0502a9965af0af02d1a
dragonjump <116328950+dragonjump@users.noreply.github.com>
2025-03-08 10:21:29 +0800

    checkpint ; can display legend n marker in desc

--- build-your-own-radar-master/src/graphing/components/search.js
+++ build-your-own-radar-master/src/graphing/components/search.js
@@ -3,7 +3,43 @@ const d3 = require('d3')
 const AutoComplete = require('../../util/autoComplete')
 const { selectRadarQuadrant, removeScrollListener } = require('../components/quadrants')
 
+function renderLegend(radarHeader) {
+  const legendContainer = radarHeader.append('div').classed('legend-container', true)
+  
+  const legendItems = [
+    { text: 'STRATEGIC', image: '/images/star.svg' },
+    { text: 'COMPANY1', image: '/images/company1.svg' },
+    { text: 'COMPANY2', image: '/images/company2.svg' },
+    { text: 'COMPANY3', image: '/images/company3.svg' }
+  ]
+
+  legendItems.forEach(item => {
+    const itemDiv = legendContainer.append('div')
+      .classed('legend-item', true)
+      .style('margin', '3px 0')
+      .style('display', 'flex')
+      .style('align-items', 'center')
+
+    itemDiv.append('img')
+      .attr('src', item.image)
+      .attr('alt', item.text)
+      .style('width', '14px')
+      .style('height', '14px')
+      .style('vertical-align', 'middle')
+
+    itemDiv.append('small')
+      .classed('legend-icon', true)
+      .style('font-size', '10px')
+      .style('margin-left', '3px')
+      .style('color', '#666')
+      .text(item.text)
+  })
+}
+
 function renderSearch(radarHeader, quadrants) {
+  // Add legend before search container
+  renderLegend(radarHeader)
+
   const searchContainer = radarHeader.append('div').classed('search-container', true)
 
   searchContainer
--- build-your-own-radar-master/src/util/inputSanitizer.js
+++ build-your-own-radar-master/src/util/inputSanitizer.js
@@ -5,10 +5,12 @@ const _ = {
 
 const InputSanitizer = function () {
   var relaxedOptions = {
-    allowedTags: ['b', 'i', 'em', 'strong', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'ul', 'br', 'p', 'u', 'img'],
+    allowedTags: ['b', 'i', 'em', 'strong', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'ul', 'br', 'p', 'u', 'img', 'small', 'span'],
     allowedAttributes: {
       a: ['href', 'target', 'rel'],
-      img: ['src', 'alt', 'style']
+      img: ['src', 'alt', 'style'],
+      small: ['class', 'style'],
+      span: ['class', 'style']
     },
   }
 
@@ -28,6 +30,37 @@ const InputSanitizer = function () {
     return processedBlip
   }
 
+  function generateLegendHTML() {
+    return `
+      <div class="legend-container" style="margin-bottom: 15px; font-size: 10px;">
+        <div class="legend-item" style="margin: 3px 0;">
+          <span class="legend-wrapper">
+            <img src="/images/star.svg" alt="Strategic Direction" style="width: 14px; height: 14px; vertical-align: middle;"/>
+            <small class="legend-icon" style="font-size: 10px; margin-left: 3px; color: #666;">STRATEGIC</small>
+          </span>
+        </div>
+        <div class="legend-item" style="margin: 3px 0;">
+          <span class="legend-wrapper">
+            <img src="/images/company1.svg" alt="Used by Company 1" style="width: 14px; height: 14px; vertical-align: middle;"/>
+            <small class="legend-icon" style="font-size: 10px; margin-left: 3px; color: #666;">COMPANY1</small>
+          </span>
+        </div>
+        <div class="legend-item" style="margin: 3px 0;">
+          <span class="legend-wrapper">
+            <img src="/images/company2.svg" alt="Used by Company 2" style="width: 14px; height: 14px; vertical-align: middle;"/>
+            <small class="legend-icon" style="font-size: 10px; margin-left: 3px; color: #666;">COMPANY2</small>
+          </span>
+        </div>
+        <div class="legend-item" style="margin: 3px 0;">
+          <span class="legend-wrapper">
+            <img src="/images/company3.svg" alt="Used by Company 3" style="width: 14px; height: 14px; vertical-align: middle;"/>
+            <small class="legend-icon" style="font-size: 10px; margin-left: 3px; color: #666;">COMPANY3</small>
+          </span>
+        </div>
+      </div>
+    `;
+  }
+
   var self = {}
   self.sanitize = function (rawBlip) {
     var blip = trimWhiteSpaces(rawBlip)
@@ -35,16 +68,35 @@ const InputSanitizer = function () {
     
     // Add star image if it's a strategic direction
     if (blip.isStrategicDirection && blip.isStrategicDirection.toLowerCase() === 'true') {
-      const starImage = '<img src="star.svg" alt="Strategic Direction" style="width: 16px; height: 16px; vertical-align: middle; margin-left: 5px;">';
+      const starImage = '<br/><span class="legend-wrapper"><img src="/images/star.svg" alt="Strategic Direction" style="width: 14px; height: 14px; vertical-align: middle; margin-left: 5px;"/><small class="legend-icon" style="font-size: 10px; margin-left: 3px; color: #666;">STRATEGIC</small></span>';
       blip.description = (blip.description || '') + ' ' + starImage;
+    }
+
+    // Add company1 indicator
+    if (blip.isUsedByChildCompany1 && blip.isUsedByChildCompany1.toLowerCase() === 'true') {
+      const company1Image = '<br/><span class="legend-wrapper"><img src="/images/company1.svg" alt="Used by Company 1" style="width: 14px; height: 14px; vertical-align: middle; margin-left: 5px;"/><small class="legend-icon" style="font-size: 10px; margin-left: 3px; color: #666;">COMPANY1</small></span>';
+      blip.description = (blip.description || '') + ' ' + company1Image;
+    }
+
+    // Add company2 indicator
+    if (blip.isUsedByChildCompany2 && blip.isUsedByChildCompany2.toLowerCase() === 'true') {
+      const company2Image = '<br/><span class="legend-wrapper"><img src="/images/company2.svg" alt="Used by Company 2" style="width: 14px; height: 14px; vertical-align: middle; margin-left: 5px;"/><small class="legend-icon" style="font-size: 10px; margin-left: 3px; color: #666;">COMPANY2</small></span>';
+      blip.description = (blip.description || '') + ' ' + company2Image;
+    }
+
+    // Add company3 indicator
+    if (blip.isUsedByChildCompany3 && blip.isUsedByChildCompany3.toLowerCase() === 'true') {
+      const company3Image = '<br/><span class="legend-wrapper"><img src="/images/company3.svg" alt="Used by Company 3" style="width: 14px; height: 14px; vertical-align: middle; margin-left: 5px;"/><small class="legend-icon" style="font-size: 10px; margin-left: 3px; color: #666;">COMPANY3</small></span>';
+      blip.description = (blip.description || '') + ' ' + company3Image;
     }
     
     blip.description = sanitizeHtml(blip.description, relaxedOptions)
     blip.name = sanitizeHtml(blip.name, restrictedOptions)
     blip.isNew = sanitizeHtml(blip.isNew, restrictedOptions)
-    
     blip.isStrategicDirection = sanitizeHtml(blip.isStrategicDirection, restrictedOptions)
- 
+    blip.isUsedByChildCompany1 = sanitizeHtml(blip.isUsedByChildCompany1, restrictedOptions)
+    blip.isUsedByChildCompany2 = sanitizeHtml(blip.isUsedByChildCompany2, restrictedOptions)
+    blip.isUsedByChildCompany3 = sanitizeHtml(blip.isUsedByChildCompany3, restrictedOptions)
     blip.status = sanitizeHtml(blip.status, restrictedOptions)
     blip.ring = sanitizeHtml(blip.ring, restrictedOptions)
     blip.quadrant = sanitizeHtml(blip.quadrant, restrictedOptions)
@@ -60,7 +112,10 @@ const InputSanitizer = function () {
     const nameIndex = header.indexOf('name')
     const isNewIndex = header.indexOf('isNew')
     const isStrategicDirectionIndex = header.indexOf('isStrategicDirection')
-     const statusIndex = header.indexOf('status')
+    const isUsedByChildCompany1Index = header.indexOf('isUsedByChildCompany1')
+    const isUsedByChildCompany2Index = header.indexOf('isUsedByChildCompany2')
+    const isUsedByChildCompany3Index = header.indexOf('isUsedByChildCompany3')
+    const statusIndex = header.indexOf('status')
     const quadrantIndex = header.indexOf('quadrant')
     const ringIndex = header.indexOf('ring')
     let description = '';
@@ -76,10 +131,13 @@ const InputSanitizer = function () {
     } catch (e) {
       console.log('error', e);
       description = descriptionIndex === -1 ? '' : blip[descriptionIndex]
-
     }
+    
     const name = nameIndex === -1 ? '' : blip[nameIndex]
     const isStrategicDirection = isStrategicDirectionIndex === -1 ? '' : blip[isStrategicDirectionIndex]
+    const isUsedByChildCompany1 = isUsedByChildCompany1Index === -1 ? '' : blip[isUsedByChildCompany1Index]
+    const isUsedByChildCompany2 = isUsedByChildCompany2Index === -1 ? '' : blip[isUsedByChildCompany2Index]
+    const isUsedByChildCompany3 = isUsedByChildCompany3Index === -1 ? '' : blip[isUsedByChildCompany3Index]
     const isNew = isNewIndex === -1 ? '' : blip[isNewIndex]
     const status = statusIndex === -1 ? '' : blip[statusIndex]
     const ring = ringIndex === -1 ? '' : blip[ringIndex]
@@ -87,19 +145,44 @@ const InputSanitizer = function () {
 
     // Add star image if it's a strategic direction
     if (isStrategicDirection && isStrategicDirection.toLowerCase() === 'true') {
-      const starImage = '<br/><img src="/images/star.svg" alt="Strategic Direction" style="width: 16px; height: 16px; vertical-align: middle; margin-left: 5px;"/><SMALL>STRATEGIC</SMALL>';
+      const starImage = '<br/><span class="legend-wrapper"><img src="/images/star.svg" alt="Strategic Direction" style="width: 14px; height: 14px; vertical-align: middle; margin-left: 5px;"/><small class="legend-icon" style="font-size: 10px; margin-left: 3px; color: #666;">STRATEGIC</small></span>';
       description = description + ' ' + starImage;
+    }
+
+    // Add company1 indicator
+    if (isUsedByChildCompany1 && isUsedByChildCompany1.toLowerCase() === 'true') {
+      const company1Image = '<br/><span class="legend-wrapper"><img src="/images/company1.svg" alt="Used by Company 1" style="width: 14px; height: 14px; vertical-align: middle; margin-left: 5px;"/><small class="legend-icon" style="font-size: 10px; margin-left: 3px; color: #666;">COMPANY1</small></span>';
+      description = description + ' ' + company1Image;
+    }
+
+    // Add company2 indicator
+    if (isUsedByChildCompany2 && isUsedByChildCompany2.toLowerCase() === 'true') {
+      const company2Image = '<br/><span class="legend-wrapper"><img src="/images/company2.svg" alt="Used by Company 2" style="width: 14px; height: 14px; vertical-align: middle; margin-left: 5px;"/><small class="legend-icon" style="font-size: 10px; margin-left: 3px; color: #666;">COMPANY2</small></span>';
+      description = description + ' ' + company2Image;
+    }
+
+    // Add company3 indicator
+    if (isUsedByChildCompany3 && isUsedByChildCompany3.toLowerCase() === 'true') {
+      const company3Image = '<br/><span class="legend-wrapper"><img src="/images/company3.svg" alt="Used by Company 3" style="width: 14px; height: 14px; vertical-align: middle; margin-left: 5px;"/><small class="legend-icon" style="font-size: 10px; margin-left: 3px; color: #666;">COMPANY3</small></span>';
+      description = description + ' ' + company3Image;
     }
 
     blip.description = sanitizeHtml(description, relaxedOptions)
     blip.name = sanitizeHtml(name, restrictedOptions)
     blip.isNew = sanitizeHtml(isNew, restrictedOptions)
     blip.isStrategicDirection = sanitizeHtml(isStrategicDirection, restrictedOptions)
+    blip.isUsedByChildCompany1 = sanitizeHtml(isUsedByChildCompany1, restrictedOptions)
+    blip.isUsedByChildCompany2 = sanitizeHtml(isUsedByChildCompany2, restrictedOptions)
+    blip.isUsedByChildCompany3 = sanitizeHtml(isUsedByChildCompany3, restrictedOptions)
     blip.status = sanitizeHtml(status, restrictedOptions)
     blip.ring = sanitizeHtml(ring, restrictedOptions)
     blip.quadrant = sanitizeHtml(quadrant, restrictedOptions)
 
     return blip
+  }
+
+  self.generateLegend = function() {
+    return sanitizeHtml(generateLegendHTML(), relaxedOptions);
   }
 
   return self
